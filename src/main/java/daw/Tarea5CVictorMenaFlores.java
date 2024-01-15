7a5fd53bddef2f259ba792bf514146b44010ebf5
dnavmoy <dnavmoy119@g.educaand.es>
2024-01-15 09:59:35 +0100

    C5: main con varios objetos

--- src/main/java/daw/Tarea5CVictorMenaFlores.java
+++ src/main/java/daw/Tarea5CVictorMenaFlores.java
@@ -12,5 +12,11 @@ public class Tarea5CVictorMenaFlores {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        Aviones b17 = new Aviones("B17", "estadounidense", 80);
+        Aviones bf109 = new Aviones("bf109", "aleman",21);
+        
+        System.out.println(b17);
+        System.out.println(bf109);
     }
 }
